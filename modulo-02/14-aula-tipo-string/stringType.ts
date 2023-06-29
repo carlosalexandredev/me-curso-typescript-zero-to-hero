968e1bef13fcf1b0010d2d74e59545fb2c85a5d0
carlosalexandredev <carlosalexandredev.contato@gmail.com>
2023-06-29 11:45:16 -0300

    Demo - Tipo Array #16

---
+++ modulo-02/14-aula-tipo-string/stringType.ts
@@ -0,0 +1,12 @@
+// Single Quotes
+let nomeCompleto: string = 'Carlos Alexandre';
+console.log(nomeCompleto);
+
+// Double Quotes
+let funcao: string = "Programador PL";
+console.log(funcao);
+
+// Back Ticks
+let nomeEmpresa: string = 'BB';
+let dadosFuncionario: string = `Seja bem vindo(a) ${nomeCompleto.toUpperCase()}, seu cargo é ${funcao} na empresa ${nomeEmpresa}!`;
+console.log(dadosFuncionario);
