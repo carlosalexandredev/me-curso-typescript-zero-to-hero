cf2caaa8c2647f01a22fe080b93cdbd9b135f5f6
carlosalexandredev <carlosalexandredev.contato@gmail.com>
2023-07-02 17:07:41 -0300

    Demo - switch...case #37

---
+++ modulo-03/aula-37-switch-case/switch-case.ts
@@ -0,0 +1,19 @@
+export { };
+
+// ==> Exemplo 01 - Uso do switch...case
+let flor = 'tulipa';
+switch (flor) {
+    case 'rosa':
+        console.log('Rosas são vermelhas');
+        break;
+    case 'violeta':
+        console.log('Violetas são azuis');
+        break;
+    case 'tulipa':
+        console.log('Tulipa são brancas');
+        break;
+    default:
+        console.log('Flor inválida');
+        break;
+}
+
